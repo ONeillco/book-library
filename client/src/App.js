f99d494f05e078b08d7b3a3cd38e83d3d1fdf538
Cory <oneillco117@gmail.com>
2021-12-17 20:58:51 -0500

    safty net

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/Home';
-import Nav from './components/Nav';
-import { UserProvider } from './context/user';
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Nav from './components/Nav'
+import {UserProvider } from './context/user'
 
-function App() {
+
+function App(props) {
+
   return (
-    
-      <Router>
-      <div>
-<UserProvider>
-  <Nav />
-  <Routes>
-  <Route exact path="/" element={<Home />} />
-  </Routes>
-</UserProvider>
+    <div>
+      <UserProvider>
+        <Nav />
+        <Routes>
+          <Route exact path="/" element={<Home />} /> 
+        </Routes>
+      </UserProvider>
     </div>
-    </Router>
-     
-    
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/context/user.js
+++ client/src/context/user.js
@@ -1,46 +1,15 @@
+//src/context/user.js
 import React, { useState, useEffect } from "react"
-// import { useNavigate } from 'react-router-dom'
 
 const UserContext = React.createContext()
 
 function UserProvider({ children }) {
-  const [user, setUser] = useState({})
-  // const [loggedIn, setLoggedIn] = useState(false)
-
-
-  useEffect(() => {
-    fetch('/me')
-    .then(res => res.json())
-    .then(data => {
-      setUser(data)
-    })
-  }, [])
-
-
-  const login = () => {
-    // setUser(user)
-   
-    // setLoggedIn(true)
-  }
-  
-  const logout = () => {
-    // setUser({})
-   
-    // setLoggedIn(false)
-  }
-  
-  const signup = () => {
-    // setUser(user)
-    
-    // setLoggedIn(true)
-  }
-
 
   return (
-    <UserContext.Provider value={{ user }}>
-    { children }
-  </UserContext.Provider>
+    <UserContext.Provider value={{}}>
+        {children}
+    </UserContext.Provider>
   )
 }
 
-export { UserContext, UserProvider }
+export { UserContext, UserProvider}
