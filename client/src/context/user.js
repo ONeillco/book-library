107a85678b8f38565487fc5961a093c648466f07
Cory <oneillco117@gmail.com>
2021-12-17 21:06:16 -0500

    saftey net

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -4,9 +4,18 @@ import React, { useState, useEffect } from "react"
 const UserContext = React.createContext()
 
 function UserProvider({ children }) {
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    fetch('/me')
+    .then(res => res.json())
+    .then(data => {
+      setUser(data)
+    })
+  }, [])
 
   return (
-    <UserContext.Provider value={{}}>
+    <UserContext.Provider value={{user}}>
         {children}
     </UserContext.Provider>
   )
